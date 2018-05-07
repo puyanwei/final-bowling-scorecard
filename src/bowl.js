e8b01210884853257024022ea09914260344353c
Puyan Wei <puyanwei@gmail.com>
2018-05-07 22:21:52 +0100

    Wrote test for inputting first or second bowl

---
+++ src/bowl.js
@@ -0,0 +1,7 @@
+'use strict';
+
+class Bowl {
+    constructor(score) {
+        this.score = score;
+    }
+}
--- test/bowl-test.js
+++ test/bowl-test.js
@@ -5,8 +5,12 @@ describe('#Bowl', () => {
 
     describe('initialize', () => {
         it('Inputs the score of the bowl', () => {
-            bowl = new Bowl(5);
+            bowl = new Bowl(5, 2);
             expect(bowl.score).toEqual(5);
+        });
+        it('Inputs whether it was the first or second bowl', () => {
+            bowl = new Bowl(6, 1);
+            expect(bowl.index).toEqual(1);
         });
     });
 });
