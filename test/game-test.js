c3885726e81011b46453250e81c81d15b1ab6cd3
Puyan Wei <puyanwei@gmail.com>
2018-05-07 19:04:33 +0100

    Created class Game using ES6 constructor function

--- test/game-test.js
+++ test/game-test.js
@@ -1,12 +1,13 @@
 describe('#Game', () => {
     beforeEach(() => {
-        let game = new Game();
+        game = new Game();
     });
 
     describe('initialize', () => {
         it('Creates an empty scorecard array', () => {
-            expect(game.scorecard).toBe([]);
+            expect(game.scorecard).toEqual(Array(0));
             expect(game.scorecard.length).toEqual(0);
+            console.log(game.scorecard);
         });
     });
 });
